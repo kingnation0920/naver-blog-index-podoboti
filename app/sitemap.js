ac9527165ae8cd23c8ec3ff72f5ae42623e7760c
kingnation0920 <57610017+kingnation0920@users.noreply.github.com>
2026-06-17 08:25:49 +0900

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://naver-blog-index-podoboti.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'hourly',
+      priority: 1,
+    },
+  ];
+}
